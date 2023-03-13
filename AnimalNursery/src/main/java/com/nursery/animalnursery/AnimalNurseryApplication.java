077c7f54006d304e78dab9d43f30d3bb7c4e1047
AlexLeXX <alexlexx444@yandex.ru>
2023-03-13 23:42:49 +0500

    some

---
+++ AnimalNursery/src/main/java/com/nursery/animalnursery/AnimalNurseryApplication.java
@@ -0,0 +1,14 @@
+package com.nursery.animalnursery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalNurseryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalNurseryApplication.class, args);
+        System.out.println("Hi !!!");
+    }
+
+}
---
+++ AnimalNursery/src/test/java/com/nursery/animalnursery/AnimalNurseryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nursery.animalnursery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalNurseryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
